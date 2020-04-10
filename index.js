ea552ad8c39db47d814fb3596786bdb886afffc6
SilicateWielder <oculartide@gmail.com>
2020-04-10 18:54:26 -0500

    Implemented automatic config loading and generation
    Configuration loading has been implemented with the ability to generate default config files on first launch, this should made bots using guildleus frienlier for new users drying to develop with it

--- index.js
+++ index.js
@@ -1,160 +1,55 @@
-// DungeonMeister Plus, written by SilicateWielder.
-global.configPath = './config.json';
-global.credentialPath = './credentials.json';
-
-// Load in dependencies.
-const fs = require('fs');
-const colors = require('colors');
+// Core Dependencies
 const Discord = require('discord.js');
+const fs = require('fs');
 
-// Load in custom dependencies.
-const display = require('./lib/display.js'); //Required for the console UI.
+// Aditional dependencies
+const configLoader = require('./lib/configloader.js');
 
-// Load in configuration file.
-global.config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-global.config = Object.assign({}, global.config, JSON.parse(fs.readFileSync(credentialPath, 'utf8')));
-global.dbLoginParameters = config.dbLoginParameters;
-ErrLog.log(JSON.stringify(global.config));
-ErrLog.log("Configuration loaded...\n");
+/* Define the defaults for our configuration files.
+We do this so that, if a guildleus bot, or the core is downloaded and run for the first time
+the user would be able to configure the bot with minimal hassle.
 
-global.cmdWrapper = require("./lib/wrappers/cmdWrapper.js");
-global.status = require("./lib/wrappers/statWrapper.js");
+the key is the path of the file, and the value is the default for  that configuration file
+*/
+let defaultConfigs = {};
+defaultConfigs['./config.properties'] = `# Complete all of the property files in the directory and then come back and set this to true.
+# The bot will not work unless this is filled out.
+complete = false
 
-// Load in database APIs
-const dbMain = require("./lib/dbMain.js");
-const dbUser = require("./lib/dbUser.js");
+# Set this to false if you're running for a release build.
+dev-mode = true
 
-// Important variables.
-global.commands = {};
-let commandsCount = 0;
-let ready = false;
+# Prefix that the bot responds to, this can be a string.
+prfix = ~
 
-// Update the version shown on the status panel.
-updateStatus('Version', config.version);
-
-// Alert the admin if we're in developer mode and update the mode status.
-if (config.devMode)
-{
-	ErrLog.log("[Starting in developer mode...]\n".bold.red);
-    updateStatus('Mode', ' Development '.bgRed);
-}
-
-// Initialize DiscordJS.
-ErrLog.log("Initializing GuildMeister...");
-global.client = new Discord.Client();
-
-// Load in commands.
-let commandsTemp = cmdWrapper.findCommands(config.commandPath);
-global.commands = commandsTemp[0];
-commandsCount = commandsTemp[1];
-
-cmdWrapper.initCommands(global.commands);
-
-// This Needs to be globally accessible.
-// Otherwise bugs cannot be submitted through the bot.
-global.channels = {};
+# Name of the bot
+name = SomeBot`;
 
 
-client.on("ready", async () => {
-	await dbMain.registerServers(client)
-	ready = true;
-	
-	status.set('default');
-	
-	// Retrieve channels.
-	channels["logs"] = client.channels.get(config.channelsLog);
-	channels["bugs"] = client.channels.get(config.channelsBug);
-	channels["features"] = client.channels.get(config.channelsFeatures);
+defaultConfigs['./tokens.properties'] = `# Login token to use with release versions
+main-Token = 1234567890SomeTokenHereABCDEFGHJKLMNOPQRSTUVWXYZ
 
-	updateStatus('Users', client.users.size);
-	updateStatus('Channels', client.channels.size);
-	updateStatus('Guilds', client.guilds.size);
+# Login token to use with testing versions
+test-Token = 1234567890SomeTokenHereABCDEFGHJKLMNOPQRSTUVWXYZ`;
+
+// Now, we can trigger the configuration loader to run, 
+let config = configLoader.loadAll(defaultConfigs);
+
+
+/*
+// Create the client.
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", async message => {
-	if(ready)
-	{
-		// If the message is from the bot or another bot, ignore it.
-		if(message.author.bot)
-		{
-			return;
-		}
-		
-		else
-		{
-			dbUser.registerUser(message.member, true)
-		}
-		
-		// Run a check to register the server.
-	    let justRegistered = await dbMain.registerServer(message.guild, true);
-		
-		if(justRegistered)
-		{
-			message.channel.send("It seems this is your first time using me on this server,\nyou'll need use the 'set channel' command to assign channels for certain commands.\n\nAdditionally, to automatically recieve news/notices/updates please run `~set channel news` within your news/announcements channel. \n\nFor help, contact <@270404393411674115>");
-		}
-		
-		// If the message does not have a prefix, ignore it.
-		if(message.content.indexOf(config.cmdPref) !== 0) return;
-		
-		// Grab all commands in message.
-		let syntaxRaw = message.content.split(config.cmdPref);
-		
-		// Process commands.
-		for(c = 1; c < syntaxRaw.length; c++)
-		{		
-			const argsRaw = syntaxRaw[c]; // Full command
-			const args = argsRaw.trim().split(/ +/g); // Arguments.
-			const syntax = args.shift().toLowerCase(); // Command.
-			
-			// Log the command sent in our private channel.
-			channels.logs.send("***COMMAND***: " + message.author + " sent command `" + argsRaw + "` in channel `" + message.channel.name + "` on server `" + message.channel.guild.name + "`");
-			
-			
-			let commandData = {
-					'Discord': Discord,
-					"client": client,
-					"config": config,
-					"message": message,
-					"syntax": syntax,
-					"args": args,
-					"argsRaw": argsRaw,
-					"channels": channels,
-					"commandCount": commands.length
-			};
-			cmdWrapper.execute(commands, syntax, commandData);
-		}
-	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-// New member? Send out a welcome message if there is one.
-client.on("guildMemberAdd", async member => {
-	if(ready)
-	{
-		let commandData = {
-			'Discord': Discord,
-			"client": client,
-			"config": config,
-			"member": member
-		}
-		//cmdWrapper.execute(commands, 'welcome', commandData);
-	}
-});
 
-// Reconnect to discord if disconnected.
-client.on('disconnected', function() {
-	// Log onto Discord.
-	if(config.devMode)
-	{
-		client.login(config.devToken);
-	} else {
-		client.login(config.token);
-	}
-});
-
-// Log onto Discord.
-if(config.devMode)
-{
-	client.login(config.devToken);
-} else {
-	client.login(config.token);
-}
+console.log(`Using token ${loginToken}`);
+client.login(loginToken);*/
